9744335b945cdd82bab1d898440865b2c2076e31
Mr.Sail <815245157@qq.com>
2018-07-30 16:28:04 +0800

    返回信息整理

---
+++ group4/src/main/java/com/example/demo/Const/MESSAGE.java
@@ -0,0 +1,27 @@
+package com.example.demo.Const;
+
+public class MESSAGE {
+
+    public static final String QUERY_SUC = "查询成功";
+
+    public static final String QUERY_ERR = "查询异常";
+
+    public static final String MODIFY_SUC = "修改成功";
+
+    public static final String MODIFY_ERR= "修改异常";
+
+    public static final String FROZE_SUC = "关闭成功";
+
+    public static final String FROZE_ERR = "关闭异常";
+
+    public static final String INSERT_SUC = "添加成功";
+
+    public static final String INSERT_ERR = "创建异常";
+
+    public static final String ALREADY_EXIS = "目标已存在";
+
+    public static final String DEL_SUC = "删除成功";
+
+    public static final String DEL_ERR = "删除异常";
+
+}
