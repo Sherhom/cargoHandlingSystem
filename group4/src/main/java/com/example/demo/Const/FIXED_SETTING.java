1b5b7cf0dfd155c2bce5b71ecf61bc1fed7ead5d
Mr.Sail <815245157@qq.com>
2018-07-30 16:27:39 +0800

    设置方面整理

---
+++ group4/src/main/java/com/example/demo/Const/FIXED_SETTING.java
@@ -0,0 +1,33 @@
+package com.example.demo.Const;
+
+public class FIXED_SETTING {
+
+    public static final String VIS_PERM = "static-access";
+
+    public static final String JSON_PRODUCE = "application/json; charset=utf-8";
+
+    public static final String CARGO_NAME = "cargoName";
+
+    public static final String KEY = "keyword";
+
+    public static final String CARGO_ID = "cargoId";
+
+    public static final String SKU_COLOR = "skuColor";
+
+    public static final String SKU_TYPE = "skuType";
+
+    public static final String WAREHOUSE_ID = "whId";
+
+    public static final String AMOUNT = "amount";
+
+    public static final String SKU_ID = "skuId";
+
+    public static final String WAREHOUSE_NAME = "whName";
+
+    public static final String WAREHOUSE_ADDR = "whAddress";
+
+    public static final String NEW_NAME = "newName";
+
+    public static final String NEW_ADDR = "newAddress";
+
+}
