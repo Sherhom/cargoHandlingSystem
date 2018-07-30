d4948c7a7cbc0d0b9fd49bec740c229ce4b55c08
Mr.Sail <815245157@qq.com>
2018-07-30 16:28:31 +0800

    路径整理

---
+++ group4/src/main/java/com/example/demo/Const/URL.java
@@ -0,0 +1,31 @@
+package com.example.demo.Const;
+
+public class URL {
+
+    public static final String ADD_CARGO = "/savaCargo";
+
+    public static final String DEL_CARGO = "/deleteCargo";
+
+    public static final String QUEY_CARGO = "/selectCargo";
+
+    public static final String UPDT_CARGO = "/updateCargo";
+
+    public static final String ADD_SKU = "/saveSku";
+
+    public static final String DEL_SKU = "/deleteSku";
+
+    public static final String DEL_SKU_BYCARGO = "/deleteSkuByCargo";
+
+    public static final String UPDT_SKU = "/changeSku";
+
+    public static final String QUEY_SKU = "/SelectSku";
+
+    public static final String ADD_WARE = "/saveWarehouse";
+
+    public static final String DEL_WARE = "/deleteWarehouse";
+
+    public static final String QUEY_WARE = "/selectWarehouse";
+
+    public static final String UPDT_WARE = "/changeWarehouse";
+    
+}
